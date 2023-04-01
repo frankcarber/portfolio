f00a150f348ca62329772bc501addddbc25e72a9
Frank Carber <existentialmomblog@gmail.com>
2023-04-01 12:09:21 -0600

    Main

--- src/components/About.js
+++ src/components/About.js
@@ -12,7 +12,8 @@ export default function About() {
           </div>
 
           <div className='bg-image hover-zoom'>
-          <div class="tenor-gif-embed" data-postid="22025093" data-share-method="host" data-aspect-ratio="1.04918" data-width="100%"><a href="https://tenor.com/view/lpscurse-gif-22025093">Lpscurse Sticker</a>from <a href="https://tenor.com/search/lpscurse-stickers">Lpscurse Stickers</a></div> <script type="text/javascript" async src="https://tenor.com/embed.js"></script>          </div>
+            <img src='https://i.pinimg.com/originals/ed/76/df/ed76df1b5da78ca7317a01cf9a648d0c.gif' style={{width:400, height:300}}/>
+          </div>
 
           <div className="centered">
             <a href = "#project-anchor">
