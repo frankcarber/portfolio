a078fb1e13bccdaa933cab934baf35ef37ce7b0a
Frank Carber <existentialmomblog@gmail.com>
2023-04-01 12:14:01 -0600

    Main

--- src/components/About.js
+++ src/components/About.js
@@ -10,7 +10,8 @@ export default function About() {
               inspired by nostalgic // irreverant visual design.
             </p>
           </div>
-
+          <div class="tenor-gif-embed" data-postid="22025096" data-share-method="host" data-aspect-ratio="0.975" data-width="100%"><a href="https://tenor.com/view/lpscurse-gif-22025096">Lpscurse Sticker</a>from <a href="https://tenor.com/search/lpscurse-stickers">Lpscurse Stickers</a></div> <script type="text/javascript" async src="https://tenor.com/embed.js"></script>
+          
           <div className='bg-image hover-zoom'>
             <img src='https://i.pinimg.com/originals/ed/76/df/ed76df1b5da78ca7317a01cf9a648d0c.gif' style={{width:400, height:300}}/>
           </div>
